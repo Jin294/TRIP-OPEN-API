4e7c4954bb2ab298ba93a998bf60fbb2939072d1
강현곤 <shoot_1@naver.com>
2023-10-25 01:58:40 +0900

    feat: 호텔 스크래핑 -이름에 뱃지제거, 가격,주소,위경도, 이미지

--- hotel/web-scraping/prac.py
+++ hotel/web-scraping/prac.py
@@ -10,6 +10,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 # selenium으로 키를 조작하기 위한 import
 from selenium.webdriver.common.keys import Keys
+# 웹 페이지에서 원하는 요소를 찾지 못했을 때 발생하는 예외
+from selenium.common.exceptions import NoSuchElementException
 # 페이지 로딩을 기다리는데에 사용할 time 모듈 import
 import time
 # 크롬 드라이버 autoinstaller
@@ -44,9 +46,11 @@ df = pd.read_csv(csv_file_path, encoding='utf-8')
 for index, row in df.iterrows():
     id = row['id']
     addr1 = row['addr1']
-    addr2 = row['addr2']    
-
-    # 검색창을 여는 버튼을 찾아 # 검색어 창
+    addr2 = row['addr2'] 
+       
+    if index == 200: break
+    
+    # 검색창을 여는 버튼, 검색창
     search_open_btn = driver.find_element(By.CSS_SELECTOR, 'button.btn_srch.srch_open')
     search_box = driver.find_element(By.CLASS_NAME, 'srch_bar').find_element(By.TAG_NAME, 'input')
 
@@ -58,20 +62,49 @@ for index, row in df.iterrows():
     search_box.send_keys(addr2)
     search_box.send_keys(Keys.RETURN)
     time.sleep(3)
- 
     # 검색 결과 확인 
     search_results = driver.find_elements(By.CLASS_NAME, 'list_4')
     if search_results:
-        # 검색 결과가 있을 때 contentId 및 숙소 정보 추출
-        for result in search_results:
+        # 검색 결과가 있을 때 id 및 숙소 정보 추출
+        for idx, result in enumerate(search_results):
             # 숙소 정보: 숙소 이름, 숙소 타입, 주소, 별점, 사진, 가격
+            # 숙소 이름
             accommodation_name = result.find_element(By.CLASS_NAME, 'name').find_element(By.TAG_NAME, 'strong').text
+            # 만약 숙소이름 옆에 뱃지가 있다면
+            try:
+                if result.find_element(By.CLASS_NAME, 'name').find_element(By.TAG_NAME, 'strong').find_element(By.CLASS_NAME, 'badge'):
+                    parts = accommodation_name.split("\n")
+                    modified_string = " ".join(parts[1:]) # 첫째 원소 제외 나머지 부분 선택
+                    accommodation_name = modified_string
+            except NoSuchElementException:
+                accommodation_name 
+            # 숙소 타입
             accommodation_type = result.find_element(By.CLASS_NAME, 'right_badges').text
-            accommodation_addr = 
-            accommodation_score = result.find_element(By.CLASS_NAME, 'score').find_element(By.TAG_NAME,'em').text
-            accommodation_pic = 
-            accommodation_price =
-            results.append({'id': id, 'accommodation_name': accommodation_name})
+            # 숙소 주소
+            accommodation_addr = result.find_element(By.XPATH, f'//*[@id="poduct_list_area"]/ul/li[{idx + 1}]/a/div/div[2]/p[2]').text
+            # 숙소 별점
+            ## 없으면 -1
+            try:
+                accommodation_score = result.find_element(By.CLASS_NAME, 'score').find_element(By.TAG_NAME,'em').text
+            except NoSuchElementException:
+                accommodation_score = -1
+            # 숙소 사진
+            accommodation_pic = result.find_element(By.CLASS_NAME, 'lazy').get_attribute('src')
+            # 숙소 가격
+            ## 가격은 대실이 p[1], 숙박이 p[2]에 있음 숙박만 사용할거임
+            ## 대실 가격과 숙박 가격을 포함하는 부모 요소 찾기
+            price_parent_element = result.find_element(By.XPATH, f'//*[@id="poduct_list_area"]/ul/li[{idx + 1}]/a/div/div[3]')
+            ## p[2]가 존재하는지 확인
+            try:
+                accommodation_price = price_parent_element.find_element(By.XPATH, 'p[2]').text
+            except NoSuchElementException:
+                 ## p[2]가 존재하지 않으면 'p'에서 값을 가져옴
+                accommodation_price = price_parent_element.find_element(By.XPATH, 'p').text
+            # 숙소 위도, 경도
+            accommodation_lat = result.find_element(By.TAG_NAME, 'a').get_attribute('data-alat')
+            accommodation_lng = result.find_element(By.TAG_NAME, 'a').get_attribute('data-alng')
+            # csv에 저장할 결과
+            results.append({'id': id, 'accommodation_name': accommodation_name, 'accommodation_type': accommodation_type, 'accommodation_addr': accommodation_addr, 'accommodation_score': accommodation_score, 'accommodation_pic': accommodation_pic, 'accommodation_price': accommodation_price, 'accommodation_lat': accommodation_lat, 'accommodation_lng': accommodation_lng})
     elif driver.find_elements(By.CLASS_NAME, 'result_empty'):
         # 검색창을 여는 버튼을 찾아 # 검색어 창
         search_open_btn = driver.find_element(By.CSS_SELECTOR, 'button.btn_srch.srch_open')
@@ -87,16 +120,49 @@ for index, row in df.iterrows():
         # 검색 결과 확인
         search_results = driver.find_elements(By.CLASS_NAME, 'list_4')
         if search_results:
-            # 검색 결과가 있을 때 contentId 및 숙소 정보 추출
-            for result in search_results:
+            ## 검색 결과가 있을 때 contentId 및 숙소 정보 추출
+            for idx, result in enumerate(search_results):
+                # 숙소 정보: 숙소 이름, 숙소 타입, 주소, 별점, 사진, 가격
+                # 숙소 이름
                 accommodation_name = result.find_element(By.CLASS_NAME, 'name').find_element(By.TAG_NAME, 'strong').text
+                # 만약 숙소이름 옆에 뱃지가 있다면
+                try:
+                    if result.find_element(By.CLASS_NAME, 'name').find_element(By.TAG_NAME, 'strong').find_element(By.CLASS_NAME, 'badge'):
+                        parts = accommodation_name.split("\n")
+                        modified_string = " ".join(parts[1:]) # 첫째 원소 제외 나머지 부분 선택
+                        accommodation_name = modified_string
+                except NoSuchElementException:
+                    accommodation_name
+                # 숙소 타입
                 accommodation_type = result.find_element(By.CLASS_NAME, 'right_badges').text
-                accommodation_addr = 
-                accommodation_score = result.find_element(By.CLASS_NAME, 'score').find_element(By.TAG_NAME,'em').text
-                accommodation_pic = 
-                accommodation_price = 
-                results.append({'id': id, 'accommodation_name': accommodation_name})
-
+                # 숙소 주소
+                accommodation_addr = result.find_element(By.XPATH, f'//*[@id="poduct_list_area"]/ul/li[{idx + 1}]/a/div/div[2]/p[2]').text
+                # 숙소 별점
+                ## 없으면 -1
+                try:
+                    accommodation_score = result.find_element(By.CLASS_NAME, 'score').find_element(By.TAG_NAME,'em').text
+                except NoSuchElementException:
+                    accommodation_score = -1
+                # 숙소 사진
+                accommodation_pic = result.find_element(By.CLASS_NAME, 'lazy').get_attribute('src')
+                # 숙소 가격
+                ## 가격은 대실이 p[1], 숙박이 p[2]에 있음 숙박만 사용할거임
+                ## 대실 가격과 숙박 가격을 포함하는 부모 요소 찾기
+                price_parent_element = result.find_element(By.XPATH, f'//*[@id="poduct_list_area"]/ul/li[{idx + 1}]/a/div/div[3]')
+                ## p[2]가 존재하는지 확인
+                try:
+                    accommodation_price = price_parent_element.find_element(By.XPATH, 'p[2]').text
+                except NoSuchElementException:
+                    ## p[2]가 존재하지 않으면 'p'에서 값을 가져옴
+                    accommodation_price = price_parent_element.find_element(By.XPATH, 'p').text
+                # 숙소 위도, 경도
+                accommodation_lat = result.find_element(By.TAG_NAME, 'a').get_attribute('data-alat')
+                accommodation_lng = result.find_element(By.TAG_NAME, 'a').get_attribute('data-alng')
+                # csv에 저장할 결과
+                results.append({'id': id, 'accommodation_name': accommodation_name, 'accommodation_type': accommodation_type, 'accommodation_addr': accommodation_addr, 'accommodation_score': accommodation_score, 'accommodation_pic': accommodation_pic, 'accommodation_price': accommodation_price, 'accommodation_lat': accommodation_lat, 'accommodation_lng': accommodation_lng})
+                
+        elif driver.find_elements(By.CLASS_NAME, 'result_empty'):
+            continue
 # 검색 결과를 pandas DataFrame으로 만들고 CSV 파일에 저장
 result_df = pd.DataFrame(results)
 result_df.to_csv(output_csv_file, index=False, encoding='utf-8')
