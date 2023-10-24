217ab21e7945f8a3cfeb34da1713b3e9240113c3
홍유빈 <acrow0330@naver.com>
2023-10-24 16:05:08 +0900

    feat: 파이썬 웹 크롤링 설정 및 mongodb 설정

--- web-scraping/scrapingBFS.py
+++ web-scraping/scrapingBFS.py
@@ -5,9 +5,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from urllib.parse import unquote
+from pymongo import MongoClient
+
+from koreanCheck import remove_non_korean
+from mongoConf import saveHTML
+
+import time 
 
 import chromedriver_autoinstaller
-print(chromedriver_autoinstaller.get_chrome_version()) # 크롬드라이버 버전확인
+
+from text_similarity import textSimilarity
+
+# print(chromedriver_autoinstaller.get_chrome_version()) # 크롬드라이버 버전확인
+
+mongodb_URI = "mongodb+srv://S09P31B205:z5HxUpl4gB@ssafy.ngivl.mongodb.net/S09B31B205?authSource=admin"
+client = MongoClient(mongodb_URI)
+db = client["S09P31B205"]
+collection = db["wiki_HTML"]
 
 
 def getKeyWord(attractionName):
@@ -18,36 +33,52 @@ def getKeyWord(attractionName):
 
     depth = 0
     queue = []
-    queue.append([f"https://namu.wiki/w/{attractionName}", depth])
+    queue.append([f"https://namu.wiki/w/{attractionName}", depth, attractionName])
 
     visited = {}
+    visitedName = set()
     visited[f"https://namu.wiki/w/{attractionName}"] = 1
-
+    visitedName.add(attractionName)
     while queue:
         node = queue.pop()
-        url, curDepth = node
-        print(curDepth)
-
+        url, curDepth, name = node
 
         wd.get(url)
         totalHTML = wd.find_element(By.ID, "app")
+        time.sleep(2)
+        name = remove_non_korean(name)
+
+        if name == "":
+            continue
         #mongo db에 페이지 저장
         # 저장 로직 추후 작성
+        # saveHTML(attractionName, name, curDepth, totalHTML)
 
+        
         hrefs = totalHTML.find_elements(By.TAG_NAME, "a")
    
         for href in hrefs:
             nxtUrl = href.get_attribute("href")
-            if nxtUrl == None or curDepth == 1 or nxtUrl in visited:
-                continue
-            if nxtUrl[0:20] != "https://namu.wiki/w/":
+            if nxtUrl is None:
                 continue
 
+            nxtName = nxtUrl[20:]
+
+            nxtName = unquote(nxtName, encoding='utf-8')
+            nxtName = remove_non_korean(nxtName)
+            if nxtName == "":
+                continue
+
+            print(f"from : {name}, to : {nxtName}")
+            if nxtUrl == None or curDepth == 1 or nxtUrl in visited or nxtName in visitedName:
+                continue
+            if nxtUrl[0:20] != "https://namu.wiki/w/" or textSimilarity(name, nxtName) < 0.5:
+                continue
             # print(href.get_attribute("href"))
-            queue.append([nxtUrl, curDepth + 1])
+            print("visited success" + '\n\n')
+
+            queue.append([nxtUrl, curDepth + 1, nxtName])
             visited[nxtUrl] = 1
+            visitedName.add(nxtName)
 
-
-# s = input("검색 키워드를 입력하세요 : ")
 getKeyWord("해운대")
-
---
+++ web-scraping/text_similarity.py
@@ -0,0 +1,20 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import difflib
+
+def textSimilarity(fromStr, toStr):
+    sentences = (fromStr, toStr)
+
+    answer_bytes = bytes(fromStr, 'utf-8')
+    input_bytes = bytes(toStr, 'utf-8')
+    answer_bytes_list = list(answer_bytes)
+    input_bytes_list = list(input_bytes)
+
+    sm = difflib.SequenceMatcher(None, answer_bytes_list, input_bytes_list)
+    similar = sm.ratio()
+
+    print(similar)
+    return similar
+
+
+# textSimilarity("해운대(영화)", "해운대")
