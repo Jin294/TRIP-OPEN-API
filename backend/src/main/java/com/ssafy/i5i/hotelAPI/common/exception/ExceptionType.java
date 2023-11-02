df19e32ea0849bea67c95af7626ab8972d74b0b1
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-02 15:03:43 +0900

    feat : add JwtUtill && provider
    Former-commit-id: 3b16232e0873515576891c1e65a221bd2ae9ef91
    Former-commit-id: de3cbf22d94460ad4733a4a131d27dcfcd8ecd4e

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/exception/ExceptionType.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/exception/ExceptionType.java
@@ -10,10 +10,12 @@ public enum ExceptionType {
      * CODE : 4자리 양의 정수 (맨 앞자리는 HTTP 상태코드의 앞 글자)
      * MESSAGE : 예외 메시지
      */
-
+    //4000번은 jwt 와 관련된 에러
     JWT_TOKEN_EXPIRED(4000, "토큰이 만료되었습니다."),
-    NULL_POINT_EXCEPTION(4001, "데이터가 없습니다."),
-    SORTED_TYPE_EXCEPTION(4002,"정렬 타입이 잘못되었습니다.")
+    JWT_PARSER_FAILED(4001, "토큰 파싱에 실패했습니다."),
+    //5000번은 데이터와 관련된 에러
+    NULL_POINT_EXCEPTION(5001, "데이터가 없습니다."),
+    SORTED_TYPE_EXCEPTION(5002,"정렬 타입이 잘못되었습니다.")
     ;
 
     private final int code;
