0e24a85de6c666a24ff249d58d1ac3e892b53197
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-03 14:28:57 +0900

    fix : filter at
    Former-commit-id: 133c87d31605858927b154131815534900569c86
    Former-commit-id: c641c0e4fa69c7e968f99e19929bacecb0d05c4b

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/config/SecurityConfig.java
@@ -1,15 +1,12 @@
 package com.ssafy.i5i.hotelAPI.common.config;
 
-import com.ssafy.i5i.hotelAPI.common.userSecurity.JwtAuthenticationFilter;
+import com.ssafy.i5i.hotelAPI.common.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
