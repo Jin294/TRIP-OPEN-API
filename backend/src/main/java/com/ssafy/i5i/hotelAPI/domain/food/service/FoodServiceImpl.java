b4c53f7e84a6c1667b01e468898f3c43f006c8b6
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-07 14:36:53 +0900

    feat : food api 구현
    Former-commit-id: 4bf9d45bf2b94b952d4eaecac1f798d6eb8d7c2a
    Former-commit-id: 3faa0f119c4d9a8ead5b2c3a187b3d28a8f11060

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/service/FoodServiceImpl.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/service/FoodServiceImpl.java
@@ -1,14 +1,20 @@
 package com.ssafy.i5i.hotelAPI.domain.food.service;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.i5i.hotelAPI.common.exception.CommonException;
+import com.ssafy.i5i.hotelAPI.common.exception.ExceptionType;
+import com.ssafy.i5i.hotelAPI.domain.food.dto.request.AttractionTitleRequestDto;
 import com.ssafy.i5i.hotelAPI.domain.food.dto.response.FoodResponseDto;
 import com.ssafy.i5i.hotelAPI.domain.food.repository.FoodRepository;
 import com.ssafy.i5i.hotelAPI.domain.hotel.dto.request.AttractionCoordinateRequestDto;
-import com.ssafy.i5i.hotelAPI.domain.hotel.dto.request.AttractionNameRequestDto;
+
+import com.ssafy.i5i.hotelAPI.domain.hotel.entity.Attraction;
 import com.ssafy.i5i.hotelAPI.domain.hotel.repository.AttractionRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -24,18 +30,36 @@ public class FoodServiceImpl implements FoodService{
 	private final AttractionRepository attractionRepository;
 
 	@Override
-	public List<FoodResponseDto> getFoodFromTravle(AttractionNameRequestDto attractionNameRequestDto) {
+	public List<FoodResponseDto> getFoodFromTravle(AttractionTitleRequestDto requestDto) {
+		Attraction attraction = attractionRepository.findByTitle(requestDto.getAttractionName())
+			.orElseThrow(() -> {throw new CommonException(ExceptionType.NULL_POINT_EXCEPTION);});
 
-
-
-		return null;
+		return foodRepository.findByCoordinate(attraction.getLatitude(), attraction.getLongitude(), requestDto.getDistance())
+			.orElseThrow(() -> new CommonException(ExceptionType.NULL_POINT_EXCEPTION))
+			.stream()
+			.map(FoodResponseDto::new)
+			.sorted(getFoodComparator(requestDto.getSorted()))
+			.collect(Collectors.toList());
 	}
 
 	@Override
-	public List<FoodResponseDto> getFoodFromLngLat(AttractionCoordinateRequestDto attractionCoordinateRequestDto) {
+	public List<FoodResponseDto> getFoodFromLngLat(AttractionCoordinateRequestDto requestDto) {
+		return foodRepository.findByCoordinate(requestDto.getLatitude(), requestDto.getLongitude(), requestDto.getDistance())
+			.orElseThrow(() -> new CommonException(ExceptionType.NULL_POINT_EXCEPTION))
+			.stream()
+			.map(FoodResponseDto::new)
+			.sorted(getFoodComparator(requestDto.getSorted()))
+			.collect(Collectors.toList());
+	}
 
-
-
-		return null;
+	private Comparator<FoodResponseDto> getFoodComparator(String sortingKey) {
+		if ("STAR".equals(sortingKey)) {
+			return Comparator.comparing(FoodResponseDto::getFoodStar);
+		} else if ("DISTANCE".equals(sortingKey)) {
+			return Comparator.comparing(FoodResponseDto::getDistance);
+		} else {
+			// 기본 정렬 기준을 제공
+			return Comparator.comparing(FoodResponseDto::getId);
+		}
 	}
 }
