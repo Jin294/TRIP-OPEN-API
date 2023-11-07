e5f35a82d70f3e0483116456bec161ef35eab97f
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-07 14:55:22 +0900

    fix : food api 타입 오류 수정
    Former-commit-id: 1c2be830cd55d29d9f7e8b0ced0c3c54044b6f8b
    Former-commit-id: d32e563e7b954adf5e4261eb98759f5adcdc32d6

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/service/FoodService.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/service/FoodService.java
@@ -2,14 +2,14 @@ package com.ssafy.i5i.hotelAPI.domain.food.service;
 
 import java.util.List;
 
+import com.ssafy.i5i.hotelAPI.domain.food.dto.request.AttractionCoordiRequestDto;
 import com.ssafy.i5i.hotelAPI.domain.food.dto.request.AttractionTitleRequestDto;
 import com.ssafy.i5i.hotelAPI.domain.food.dto.response.FoodResponseDto;
-import com.ssafy.i5i.hotelAPI.domain.food.dto.response.FoodResponseDtoInterface;
-import com.ssafy.i5i.hotelAPI.domain.hotel.dto.request.AttractionCoordinateRequestDto;
+
 
 
 public interface FoodService {
 	List<FoodResponseDto> getFoodFromTravle(AttractionTitleRequestDto attractionTitleRequestDto);
 
-	List<FoodResponseDto> getFoodFromLngLat(AttractionCoordinateRequestDto attractionCoordinateRequestDto);
+	List<FoodResponseDto> getFoodFromLngLat(AttractionCoordiRequestDto attractionCoordiRequestDto);
 }
