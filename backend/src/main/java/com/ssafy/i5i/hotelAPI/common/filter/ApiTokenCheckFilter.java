561f9ef618db906575d9d5293b1c26addf6fbd4b
홍유빈 <acrow0330@naver.com>
2023-11-08 17:08:07 +0900

    fix: token filter mysql로 동기 설정
    Former-commit-id: d9fbed223d32719ef902531d19e8dabf51a07d3b
    Former-commit-id: e73fba706631d1d3031a4de0c763118125de0a22

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/filter/ApiTokenCheckFilter.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/filter/ApiTokenCheckFilter.java
@@ -48,19 +48,19 @@ public class ApiTokenCheckFilter implements Filter {
             return;
         }
 
-//        //토큰 유효성 체크, 유효한 토큰 아니면 예외처리
-        if (!tokenService.checkValidToken(token)) {
-            log.error("ApiTokenCheckFilter 52 lines, invalid token");
-            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            httpServletResponse.getWriter().write("{\n \"status_code\" : \"401\",\n \"message\" : \"The token is either invalid or has exceeded the daily usage limit.\" \n}");
-            return;
-        }
-//        if(!tokenUserService.checkValidToken(token)) {
-//            log.error("not valid");
+////        //토큰 유효성 체크, 유효한 토큰 아니면 예외처리
+//        if (!tokenService.checkValidToken(token)) {
+//            log.error("ApiTokenCheckFilter 52 lines, invalid token");
 //            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 //            httpServletResponse.getWriter().write("{\n \"status_code\" : \"401\",\n \"message\" : \"The token is either invalid or has exceeded the daily usage limit.\" \n}");
 //            return;
 //        }
+        if(!tokenUserService.checkValidToken(token)) {
+            log.error("not valid");
+            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            httpServletResponse.getWriter().write("{\n \"status_code\" : \"401\",\n \"message\" : \"The token is either invalid or has exceeded the daily usage limit.\" \n}");
+            return;
+        }
 
         chain.doFilter(request, response);
     }
