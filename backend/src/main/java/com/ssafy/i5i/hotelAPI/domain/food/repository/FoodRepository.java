f1141f60155f172a14bea896ffa52f11f37165d9
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-08 10:21:08 +0900

    fix : food api 수정
    Former-commit-id: 5b6651d5b06d01550691d426b8daae6cb7a18b96
    Former-commit-id: 54656bbde09995a780c11067e1132781f71162d9

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/repository/FoodRepository.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/repository/FoodRepository.java
@@ -15,8 +15,18 @@ import com.ssafy.i5i.hotelAPI.domain.food.entity.Food;
 
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
+	// @Query("select c from MemberChallenge c where c.member.idx = :memberIdx order by c.status asc,c.endTime asc")
+	// List<MemberChallenge> findMemberChallengeMemberIdx(@Param("memberIdx") Long memberIdx);
+
 	@Query("SELECT c FROM Food c WHERE c.attraction.contentId = :attractionId")
 	Optional<List<FoodResponseDto>> findByAttractionId(@Param("attractionId") Integer attractionId);
+
+	@Query("SELECT f FROM Food f WHERE :minX <= f.foodLongitude <= :maxX and :minY <= f. <= maxY")
+	Optional<List<FoodResponseDto>> getAroundShopList(double maxY, double maxX, double minY, double minX);
+	//
+	// "SELECT f FROM Food f " +
+	// 	"WHERE f.foodLongitude >= :minLongitude AND f.foodLongitude <= :maxLongitude " +
+	// 	"AND f.foodLatitude >= :minLatitude AND f.foodLatitude <= :maxLatitude"
 
 
 	@Query(value = "SELECT *, ST_Distance_Sphere(POINT(:longitude, :latitude), POINT(f.longitude, f.latitude)) as distance "
