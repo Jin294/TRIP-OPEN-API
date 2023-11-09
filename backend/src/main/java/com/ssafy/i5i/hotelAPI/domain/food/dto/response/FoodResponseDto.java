0351154ac62dfe2c870365d920dfd5865ec5bfc4
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-09 15:05:31 +0900

    fix : food api 수정
    Former-commit-id: f1f489e6a41698a62cf626885eaaa838c5bc7acb
    Former-commit-id: 1d12021818ac63d94735ca38838a22473f246147

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/dto/response/FoodResponseDto.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/dto/response/FoodResponseDto.java
@@ -1,35 +1,75 @@
 package com.ssafy.i5i.hotelAPI.domain.food.dto.response;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor()
-public class FoodResponseDto{
-	private Long id;
-	private String foodName;
-	private String foodType;
-	private Double foodLongitude;
-	private Double foodLatitude;
-	private Integer foodJjim;
-	private Integer foodScore;
-	private Double foodStar;
-	private Integer foodStarUser;
-	private Double Distance;
+public class FoodResponseDto {
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Title {
+		private Integer attractionId;
+		private String attracionName;
+		private Double attractionLongitude;
+		private Double attractionLatitude;
+		private Long foodId;
+		private String foodName;
+		private String foodType;
+		private Double foodLongitude;
+		private Double foodLatitude;
+		private Integer foodJjim;
+		private Integer foodScore;
+		private Double foodStar;
+		private Integer foodStarUser;
 
-	public FoodResponseDto(FoodResponseDtoInterface foodResponseDtoInterface) {
-		this.id = foodResponseDtoInterface.getId();
-		this.foodName = foodResponseDtoInterface.getFoodName();
-		this.foodType = foodResponseDtoInterface.getFoodType();
-		this.foodLongitude = foodResponseDtoInterface.getFoodLongitude();
-		this.foodLatitude = foodResponseDtoInterface.getFoodLatitude();
-		this.foodJjim = foodResponseDtoInterface.getFoodJjim();
-		this.foodScore = foodResponseDtoInterface.getFoodScore();
-		this.foodStar = foodResponseDtoInterface.getFoodStar();
-		this.foodStarUser = foodResponseDtoInterface.getFoodStarUser();
-		this.Distance = foodResponseDtoInterface.getDistance();
+		public FoodResponseDto.TitleD convertToFDto (){
+			return new FoodResponseDto.TitleD(this.attractionId, this.attracionName,
+				this.attractionLongitude,this.attractionLatitude,
+				this.foodId,this.foodName,this.foodType,
+				this.foodLongitude, this.foodLatitude, this.foodJjim, this.foodScore, this.foodStar,
+				this.foodStarUser, null);
+		}
+	}
+
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class TitleD {
+		private Integer attractionId;
+		private String attracionName;
+		private Double attractionLongitude;
+		private Double attractionLatitude;
+		private Long foodId;
+		private String foodName;
+		private String foodType;
+		private Double foodLongitude;
+		private Double foodLatitude;
+		private Integer foodJjim;
+		private Integer foodScore;
+		private Double foodStar;
+		private Integer foodStarUser;
+		private Double Distance;
+	}
+
+
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Coordi {
+		private Long id;
+		private String foodName;
+		private String foodType;
+		private Double foodLongitude;
+		private Double foodLatitude;
+		private Integer foodJjim;
+		private Integer foodScore;
+		private Double foodStar;
+		private Integer foodStarUser;
+		private Double Distance;
 	}
 }
--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/repository/FoodRepository.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/repository/FoodRepository.java
@@ -7,57 +7,63 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
+// import com.ssafy.i5i.hotelAPI.domain.food.dto.response.FoodResponseDtoInterface;
 import com.ssafy.i5i.hotelAPI.domain.food.dto.response.FoodResponseDto;
-import com.ssafy.i5i.hotelAPI.domain.food.dto.response.FoodResponseDtoInterface;
+import com.ssafy.i5i.hotelAPI.domain.food.dto.response.FoodTitleResponseDto;
+import com.ssafy.i5i.hotelAPI.domain.food.entity.AttractionFood;
 import com.ssafy.i5i.hotelAPI.domain.food.entity.Food;
 
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
-	// @Query("select c from MemberChallenge c where c.member.idx = :memberIdx order by c.status asc,c.endTime asc")
-	// List<MemberChallenge> findMemberChallengeMemberIdx(@Param("memberIdx") Long memberIdx);
-
-	// @Query("SELECT c FROM Food c WHERE c.attraction.contentId = :attractionId")
-	// Optional<List<FoodResponseDto>> findByAttractionId(@Param("attractionId") Integer attractionId);
 
 	@Query("SELECT f FROM Food f WHERE f.foodLongitude >= :minX AND f.foodLongitude <= :maxX AND f.foodLatitude >= :minY AND f.foodLatitude <= :maxY")
-	Optional<List<Food>> getAroundFoodList(double maxY, double maxX, double minY, double minX);
+	Optional<List<Food>> getFoodFromLngLatv(double maxY, double maxX, double minY, double minX);
 
+	@Query("SELECT a.contentId, a.title, a.latitude, a.longitude, " +
+		"f.id, f.foodName, f.foodType, f.foodLongitude, f.foodLatitude, " +
+		"f.foodJjim, f.foodScore, f.foodStar, f.foodStarUser " +
+		"FROM Attraction a " +
+		"LEFT JOIN AttractionFood af ON a.contentId = af.attraction.contentId " +
+		"LEFT JOIN Food f ON af.food.id = f.id " +
+		"WHERE a.title = :title")
+	Optional<List<FoodResponseDto.Title>> getFoodFromTravle(String title);
 
-	@Query(value = "SELECT f.food_id as id, f.food_name as foodName, f.food_type as foodType, "
-		+ "f.food_longitude as foodLongitude, f.food_latitude as foodLatitude, "
-		+ "f.food_jjim as foodJjim, f.food_score as foodScore, "
-		+ "f.food_star as foodStar, f.food_staruser as foodStarUser, "
-		+ "ST_Distance_Sphere(POINT(:longitude, :latitude), POINT(food_longitude, food_latitude)) as distance "
-		+ "From food as f "
-		+ "HAVING distance <= :distance", nativeQuery = true)
-	Optional<List<FoodResponseDtoInterface>> findByCoordinate(@Param("latitude") Double latitude, @Param("longitude") Double longitude,
-		@Param("distance") Long distance);
+	// @Query("SELECT a,af,f " +
+	// 	"FROM Attraction a " +
+	// 	"LEFT JOIN AttractionFood af ON a.contentId = af.attraction.contentId " +
+	// 	"LEFT JOIN Food f ON af.food.id = f.id " +
+	// 	"WHERE a.title = :title")
+	// Optional<List<AttractionFood>> getFoodFromTravle(String title);
 
+	/**
+	 *
+	 */
 
-	@Query(value = "SELECT f.food_id as id, f.food_name as foodName, f.food_type as foodType, "
-		+ "f.food_longitude as foodLongitude, f.food_latitude as foodLatitude, "
-		+ "f.food_jjim as foodJjim, f.food_score as foodScore, "
-		+ "f.food_star as foodStar, f.food_staruser as foodStarUser, "
-		+ "(6371*acos(cos(radians(:latitude))*cos(radians(food_latitude))*cos(radians(food_longitude) "
-		+ "-radians(:longitude))+sin(radians(:latitude))*sin(radians(food_latitude)))) "
-		+ "as distance "
-		+ "From food as f "
-		+ "HAVING distance <= :distance", nativeQuery = true)
-	Optional<List<FoodResponseDtoInterface>> findByCoordinate3(@Param("latitude") Double latitude, @Param("longitude") Double longitude,
-		@Param("distance") Long distance);
-
-
-	// @Query("SELECT f, "
-	// 	+ "(6371 * acos(cos(radians(:latitude)) * cos(radians(f.foodLatitude)) * cos(radians(f.foodLongitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(f.foodLatitude))) as distance) "
-	// 	+ "FROM Food f WHERE "
-	// 	+ "6371 * acos(cos(radians(:latitude)) * cos(radians(f.foodLatitude)) * cos(radians(f.foodLongitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(f.foodLatitude)) <= :distance)")
-	// Optional<List<Food>> findFoodWithinDistance(@Param("latitude") Double latitude, @Param("longitude") Double longitude, @Param("distance") Long distance);
-
-
-	@Query("SELECT f FROM Food f WHERE f.foodName = :name")
-	Food findByFoodName(@Param("name") String name);
-
-
+	// @Query(value = "SELECT f.food_id as id, f.food_name as foodName, f.food_type as foodType, "
+	// 	+ "f.food_longitude as foodLongitude, f.food_latitude as foodLatitude, "
+	// 	+ "f.food_jjim as foodJjim, f.food_score as foodScore, "
+	// 	+ "f.food_star as foodStar, f.food_staruser as foodStarUser, "
+	// 	+ "ST_Distance_Sphere(POINT(:longitude, :latitude), POINT(food_longitude, food_latitude)) as distance "
+	// 	+ "From food as f "
+	// 	+ "HAVING distance <= :distance", nativeQuery = true)
+	// Optional<List<FoodResponseDtoInterface>> findByCoordinate(@Param("latitude") Double latitude, @Param("longitude") Double longitude,
+	// 	@Param("distance") Long distance);
+	//
+	//
+	// @Query(value = "SELECT f.food_id as id, f.food_name as foodName, f.food_type as foodType, "
+	// 	+ "f.food_longitude as foodLongitude, f.food_latitude as foodLatitude, "
+	// 	+ "f.food_jjim as foodJjim, f.food_score as foodScore, "
+	// 	+ "f.food_star as foodStar, f.food_staruser as foodStarUser, "
+	// 	+ "(6371*acos(cos(radians(:latitude))*cos(radians(food_latitude))*cos(radians(food_longitude) "
+	// 	+ "-radians(:longitude))+sin(radians(:latitude))*sin(radians(food_latitude)))) "
+	// 	+ "as distance "
+	// 	+ "From food as f "
+	// 	+ "HAVING distance <= :distance", nativeQuery = true)
+	// Optional<List<FoodResponseDtoInterface>> findByCoordinate3(@Param("latitude") Double latitude, @Param("longitude") Double longitude,
+	// 	@Param("distance") Long distance);
+	//
+	//
+	// @Query("SELECT f FROM Food f WHERE f.foodName = :name")
+	// Food findByFoodName(@Param("name") String name);
 }
