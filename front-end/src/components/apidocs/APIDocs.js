f0a835d8b2f5767b8d6f24ddf719c359b6f2edc3
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-10 16:15:02 +0900

    comment : 주석 처리
    Former-commit-id: 4409cc713dea5e8bcccbc588d694e22660bf3937
    Former-commit-id: 0e4b2db1c3d61da7d3b976bf9917b2e1dcdaa986

--- front-end/src/components/apidocs/APIDocs.js
+++ front-end/src/components/apidocs/APIDocs.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import styles from "./APIDocs.module.css";
 import SideBar from "./SideBar";
 import APIContent from "./APIContent";
+import wave from "../../assets/img/wave.png";
 
 const APIDocs = () => {
-  const [docsId, setDocsId] = useState('');
+  const [docsId, setDocsId] = useState("");
   const handleDocsId = (data) => {
     console.log("main=====", data);
     setDocsId(data);
@@ -16,10 +17,9 @@ const APIDocs = () => {
         <div className={styles.boardTop}>
           <h3>오픈 API를 이용해 창의적인 애플리케이션을 제작해보세요</h3>
         </div>
-
         <div className={styles.boardContent}>
-          <SideBar onSetId={handleDocsId}/>
-          <APIContent data={docsId}/>
+          <SideBar onSetId={handleDocsId} />
+          <APIContent data={docsId} />
         </div>
       </div>
     </div>
