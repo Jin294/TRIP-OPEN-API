b3f5c006131f731af038bf3801705d3f6da884c9
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-10 17:53:41 +0900

    fix : path
    Former-commit-id: fac8d9030825f1312e4586871d59f42041f1b6b0
    Former-commit-id: e775b1ab9f5b965c61b5248ce5f5981472f94b9b

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/controller/DocsController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/controller/DocsController.java
@@ -13,18 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/docs")
+@RequestMapping("/docs/data")
 @RequiredArgsConstructor
 @Slf4j
 public class DocsController {
     private final DocsService docsService;
 
-    @GetMapping("/type")
+    @GetMapping("/apitype")
     public List<TypeResponseDto> getType(){
         return docsService.getTypeList();
     }
 
-    @GetMapping("/data/{api_type_id}")
+    @GetMapping("/apidata/{api_type_id}")
     public List<ApiDataResponseDto> getData(@PathVariable("api_type_id") Long id){
         return docsService.getApiDataList(id);
     }
