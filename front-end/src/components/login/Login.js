5c20d6ba43f289a22ab455d70d851bb9d805fd61
이진호 <blueland1001@naver.com>
2023-11-13 14:08:27 +0900

    fix: 회원가입 정보 수정
    Former-commit-id: b98e2d18ef3b7e4ea4451d6a56992adbd23af7a4
    Former-commit-id: d2c35c9e282af67742b1bfb25c4babaf7c6f8f0e

--- front-end/src/components/login/Login.js
+++ front-end/src/components/login/Login.js
@@ -41,9 +41,9 @@ const Login = () => {
             const res = await basicHttp.post(`/docs/service/login`, userData);
             console.log(res);
             console.log('로그인 성공');
-            localStorage.setItem('access-token', res.data.data['access-token']);
-            localStorage.setItem('refresh-token', res.data.data['refresh-token']);
-            dispatch(setUserInfo({ accessToken: res.data.data['access-token'] }));
+            // localStorage.setItem('access-token', res.data.data['access-token']);
+            // localStorage.setItem('refresh-token', res.data.data['refresh-token']);
+            // dispatch(setUserInfo({ accessToken: res.data.data['access-token'] }));
             console.log(userInfo);
             navigate('/');
             // alert('로그인 성공');
