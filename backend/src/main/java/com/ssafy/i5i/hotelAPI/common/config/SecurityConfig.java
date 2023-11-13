3e7fcc7e1f7cac72060118940ee1372a1230671d
홍유빈 <acrow0330@naver.com>
2023-11-13 13:07:13 +0900

    fix: cors 에러 해결
    Former-commit-id: 558b38efed2669f4e39d9debdfcdf313972e6bb9
    Former-commit-id: fc2707c7ab115c39b964745def21f14f5786dd0b

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/config/SecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.security.SecureRandom;
 
@@ -71,7 +74,19 @@ public class SecurityConfig {
         ;
         return http.build();
     }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // Allow requests from any origin
+        configuration.addAllowedMethod("*"); // Allow all HTTP methods
+        configuration.addAllowedHeader("*"); // Allow all headers
+        configuration.setAllowCredentials(true); // Allow credentials (e.g., cookies)
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 
 }
 
