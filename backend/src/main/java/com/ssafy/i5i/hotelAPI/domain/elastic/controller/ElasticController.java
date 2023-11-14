301d178ecbe5fe2330df6b9870d18f124686c531
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-14 17:31:04 +0900

    fix : add get Mapping
    Former-commit-id: 4cdbb249da80e704b28592e3abe8adaf640059b0
    Former-commit-id: 0fec92ef70b138f81d3f32589bc964f429e8a0ea

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
@@ -2,6 +2,7 @@ package com.ssafy.i5i.hotelAPI.domain.elastic.controller;
 
 import com.ssafy.i5i.hotelAPI.domain.elastic.service.ElasticService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -11,6 +12,8 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/elastic")
 public class ElasticController {
     private final ElasticService elasticService;
+
+    @GetMapping("test")
     public Mono<String> test(){
         return elasticService.test();
     }
