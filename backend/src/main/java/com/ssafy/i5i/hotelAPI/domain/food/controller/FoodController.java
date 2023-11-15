6efc93045567bde8d5f394a05f93bf9ca458381e
홍유빈 <acrow0330@naver.com>
2023-11-15 23:41:57 +0900

    fix: food controller cors 에러 해결
    Former-commit-id: 3bc6b97c323926866cb8c77a281f98c06adba08a
    Former-commit-id: c966ce83b984b98179668508a4803eedca17694c

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @RequestMapping("/api/restaurant")
 @Slf4j
+@CrossOrigin(origins = "*")
 public class FoodController {
 	private final FoodServiceImpl foodServiceImpl;
 	@GetMapping("/by-name")
