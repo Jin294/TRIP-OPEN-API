a6be46cefb1b28b7d153b1998528450b36f72ecf
이진호 <blueland1001@naver.com>
2023-11-16 10:59:51 +0900

    remove: 불필요 이미지파일 및 js파일 삭제
    Former-commit-id: ccf2e9d933ade2d66f3b3a9f3a5efcfe70962e7c
    Former-commit-id: 2b2f86be4067e0a68fc5575eace5b6289316f7aa

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,13 +10,9 @@ import Login from './components/login/Login';
 // 마이페이지
 import Mypage from './components/mypage/Mypage';
 // 서비스 소개
-import ServiceInfo from './components/serviceinfo/ServiceInfo';
 import APIDocs from './components/apidocs/APIDocs';
 // API문서
 // 예시 페이지
-import ExExchange from './components/examplepage/ExExchange';
-import ExCard from './components/examplepage/ExCard';
-import ExCardContent from './components/examplepage/ExCardContent';
 
 import { useEffect, useRef } from 'react';
 
@@ -52,12 +48,8 @@ function App() {
                 <Route path="/signup" element={<Signup />}></Route>
                 <Route path="/login" element={<Login />}></Route>
                 <Route path="/mypage" element={<Mypage />}></Route>
-                {/* <Route path="/serviceinfo" element={<ServiceInfo />}></Route> */}
                 <Route path="/apidocs" element={<APIDocs />} />
                 <Route path="/apidocs/:tab" element={<APIDocs />}></Route>
-                <Route path="/exexchange" element={<ExExchange />}></Route>
-                <Route path="/excard" element={<ExCard />}></Route>
-                <Route path="/excardcontent" element={<ExCardContent />}></Route>
                 <Route path="/*" element={<Home />} />
             </Routes>
         </div>
