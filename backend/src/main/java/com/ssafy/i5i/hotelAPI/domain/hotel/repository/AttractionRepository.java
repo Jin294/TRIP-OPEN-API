3f4a41efc3f7adbfa9392c48b7b14125601d4437
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-16 17:22:41 +0900

    fix : jpa 레파지토리에서 @query 어노테이션 추가
    Former-commit-id: 2bcf5c090b6f73fed94407a55b698515d05a87a6
    Former-commit-id: be0dcd2cd690df1ed7bb663643cb3a9bb86e4db3

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/repository/AttractionRepository.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/repository/AttractionRepository.java
@@ -2,8 +2,13 @@ package com.ssafy.i5i.hotelAPI.domain.hotel.repository;
 
 import com.ssafy.i5i.hotelAPI.domain.hotel.entity.Attraction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface AttractionRepository extends JpaRepository<Attraction, Long> {
-    Optional<Attraction> findByTitle (String title);
+
+    @Query("SELECT a FROM Attraction a WHERE a.title = :title")
+    Optional<Attraction> findByTitle(@Param("title") String title);
 }
