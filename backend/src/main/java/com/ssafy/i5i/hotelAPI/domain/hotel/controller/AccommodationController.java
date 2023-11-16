81d0abe0f17e5c0ea75952e15ebc38b956b292f0
홍유빈 <acrow0330@naver.com>
2023-11-16 10:22:10 +0900

    fix: cors에러 해결
    Former-commit-id: 8d05b7ceedb5cd4e4a6b22b72d3309f1f4c7f9bb
    Former-commit-id: 1556880d08a40c151c0e91feeccc04a1a1e4e260

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/controller/AccommodationController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/controller/AccommodationController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/accommodation")
+@CrossOrigin(origins = "*")
 public class AccommodationController {
     private final AccommodationService accommodationService;
 
