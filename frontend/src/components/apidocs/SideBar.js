4bb951b9ed5a7054049316d59a91565cfbd31ff7
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-27 15:47:38 +0900

    rename : 파일 수정
    Former-commit-id: 97bb7bcece91b79779be7c0e552d21dbf75e9a76
    Former-commit-id: d0f71e33d054e47907d4d96c5925b06c8c46cf22

--- frontend/src/components/apidocs/SideBar.js
+++ frontend/src/components/apidocs/SideBar.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import styles from './SideBar.module.css';
-import { NavLink } from 'react-router-dom';
-import basicHttp from '../../api/basicHttp';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import styles from "./SideBar.module.css";
+import { NavLink } from "react-router-dom";
+import basicHttp from "../../api/basicHttp";
+import { useParams } from "react-router-dom";
 
-<<<<<<< HEAD
 
 
 const SideBar = ({ onSetId }) => {
@@ -106,24 +105,11 @@ const SideBar = ({ onSetId }) => {
       } catch (error) {
         console.log("Error fetching API data", error);
       }
-=======
-const SideBar = ({ onSetId }) => {
-    const { tab } = useParams();
-    const [tabsData, setTabsData] = useState([]);
-    const [selectedTab, setSelectedTab] = useState('숙소 API');
-    const [selectedSub, setSelectedSub] = useState(1);
-    const setId = (data) => {
-        onSetId(data);
->>>>>>> develop-back
     };
 
-    useEffect(() => {
-        if (tab === 'restaurant') handleTabClick('음식점 API');
-        else if (tab === 'search') handleTabClick('검색 API');
-        else handleTabClick('숙소 API');
-    }, [tab]);
+    getApiDocsList();
+  }, []);
 
-<<<<<<< HEAD
   return (
     <div className={styles.sidebar}>
       <aside>
@@ -173,91 +159,6 @@ const SideBar = ({ onSetId }) => {
       </aside>
     </div>
   );
-=======
-    const handleTabClick = (tab) => {
-        setSelectedTab(tab);
-
-        if (tab === '음식점 API') handleSubClick(3);
-        else if (tab === '검색 API') handleSubClick(5);
-        else handleSubClick(1);
-    };
-
-    const handleSubClick = (sub) => {
-        setSelectedSub(sub);
-        setId(sub);
-    };
-
-    useEffect(() => {
-        const getApiDocsList = async () => {
-            try {
-                const response = await basicHttp.get(`/docs/data/apilist`);
-                const responseData = response.data;
-                console.log('responseData', responseData);
-
-                if (responseData.data) {
-                    const groupedTabs = [
-                        {
-                            title: '숙소 API',
-                            url: '/apidocs/accommodation',
-                            subTabs: responseData.data.slice(0, 2),
-                        },
-                        {
-                            title: '음식점 API',
-                            url: '/apidocs/restaurant',
-                            subTabs: responseData.data.slice(2, 4),
-                        },
-                        {
-                            title: '검색 API',
-                            url: '/apidocs/search',
-                            subTabs: responseData.data.slice(4, 5),
-                        },
-                    ];
-                    setTabsData(groupedTabs);
-                }
-            } catch (error) {
-                console.log('Error fetching API data', error);
-            }
-        };
-
-        getApiDocsList();
-    }, []);
-
-    return (
-        <div className={styles.sidebar}>
-            <aside>
-                <ul>
-                    {tabsData.map((group) => (
-                        <li key={group.title}>
-                            <h3 className={selectedTab === group.title ? styles.selectedTab : ''}>
-                                <NavLink to={group.url} onClick={() => handleTabClick(group.title)}>
-                                    <p className={selectedTab === group.title ? styles.selectedTabText : ''}>{group.title}</p>
-                                </NavLink>
-                            </h3>
-                            <ul>
-                                {selectedTab === group.title &&
-                                    group.subTabs.map((tab) => (
-                                        <li key={tab.api_data_id}>
-                                            <div
-                                                className={
-                                                    selectedSub === tab.api_data_id
-                                                        ? styles.selected
-                                                        : styles.noSelected
-                                                }
-                                                onClick={() => handleSubClick(tab.api_data_id)}
-                                            >
-                                                {tab.title}
-                                            </div>
-                    
-                                        </li>
-                                    ))}
-                            </ul>
-                        </li>
-                    ))}
-                </ul>
-            </aside>
-        </div>
-    );
->>>>>>> develop-back
 };
 
 export default SideBar;
